9004cf23254f40a10e4a9e6209a68a5069aaee67
DESKTOP-T2LTDCC\Joshua <james22@sheridancollege.ca>
2024-01-29 14:53:38 -0500

    Ball finally works as intended. Fires in intended direction regardless of aim angle and position.

--- Assets/Assignment/Scripts/Ball Mover.cs
+++ Assets/Assignment/Scripts/Ball Mover.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BallMover : MonoBehaviour
 {
+    Vector2 direction = new Vector2(0, 10);
     public float force = 10000f;
     Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.AddRelativeForce(rigidbody.position * force * Time.deltaTime);
+        rigidbody.AddRelativeForce(direction * force * Time.deltaTime);
     }
 
     // Update is called once per frame
