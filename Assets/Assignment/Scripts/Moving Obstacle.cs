e53c1335abeac57ea23a7bf2860d7bea6590614f
DESKTOP-T2LTDCC\Joshua <james22@sheridancollege.ca>
2024-01-29 15:32:57 -0500

    Final version. All obstacles placed and functioning as nintended.

---
+++ Assets/Assignment/Scripts/Moving Obstacle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    float direction = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        direction = direction * -1;
+    }
+}
